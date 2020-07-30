8126eef1f81b5c32aee68fc1fdd46026ac5c57e3
Natalya Leonovich <nleonovich@avito.ru>
2020-07-31 02:52:37 +0300

    first test homework

---
+++ java_test/java_example/src/test/java/selenium/ExampleTest.java
@@ -0,0 +1,35 @@
+package selenium;
+
+        import org.junit.After;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.chrome.ChromeDriver;
+        import org.openqa.selenium.support.ui.WebDriverWait;
+
+        import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class ExampleTest {
+
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void exampleTest() {
+        driver.navigate().to("http://localhost:8080/litecart/en/");
+        driver.findElement(By.name("email")).isDisplayed();
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
