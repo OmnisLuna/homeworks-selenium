6a2c90560f209d79a8c601121ef686a7e22f66f9
Natalya Leonovich <nleonovich@avito.ru>
2020-08-11 16:08:05 +0300

    Fix ex.10

--- java_test/java_example/src/test/java/selenium/CheckProductValuesTest.java
+++ java_test/java_example/src/test/java/selenium/CheckProductValuesTest.java
@@ -19,12 +19,13 @@ public class CheckProductValuesTest {
     public WebDriver driver;
     public WebDriverWait wait;
 
-    public boolean checkBiggerValue(String bigger, String smaller) {
-        if (bigger.compareTo(smaller) < 0) {
-            return false;
-        } else {
-            return true;
-        }
+    public boolean checkBiggerValue(float bigger, float smaller) {
+        return (bigger > smaller);
+    }
+
+    public float stringToFloat(String value) {
+        String valueFormatted = value.substring(0, value.length() - 2); //убираем px из строки - 20px
+        return Float.parseFloat(valueFormatted);
     }
 
     @Before
@@ -49,27 +50,31 @@ public class CheckProductValuesTest {
 
             WebElement productRegularPrice = product.findElement(By.className("regular-price"));
             String productRegularPriceValue = productRegularPrice.getText();
+
             //в) обычная цена зачёркнутая и серая (можно считать, что "серый" цвет это такой,
             // у которого в RGBa представлении одинаковые значения для каналов R, G и B)
-            Assert.assertEquals("rgba(119, 119, 119, 1)", productRegularPrice.getCssValue("color"));
+            Assert.assertTrue(productRegularPrice.getCssValue("color")
+                    .matches("^rgba\\((\\d{1,3}), (\\1\\b), (\\1\\b), (\\d*)\\)$")); //проверяем соответствие паттерну через regex
             Assert.assertEquals("line-through solid rgb(119, 119, 119)", productRegularPrice.getCssValue("text-decoration"));
             Assert.assertEquals("400", productRegularPrice.getCssValue("font-weight"));
             Assert.assertEquals("14.4px", productRegularPrice.getCssValue("font-size"));
 
+            WebElement productCampaignPrice = product.findElement(By.className("campaign-price"));
+            String productCampaignPriceValue = productCampaignPrice.getText();
+
             // г) акционная жирная и красная (можно считать, что "красный" цвет это такой, у которого в RGBa
             // представлении каналы G и B имеют нулевые значения)
             // (цвета надо проверить на каждой странице независимо, при этом цвета на разных страницах могут не совпадать)
-            WebElement productCampaignPrice = product.findElement(By.className("campaign-price"));
-            String productCampaignPriceValue = productCampaignPrice.getText();
-            Assert.assertEquals("rgba(204, 0, 0, 1)", productCampaignPrice.getCssValue("color"));
+            Assert.assertTrue(productCampaignPrice.getCssValue("color")
+                    .matches("^rgba\\((\\d{1,3}), ([0]), ([0]), (\\d*)\\)$")); //проверяем соответствие паттерну через regex
             Assert.assertEquals("none solid rgb(204, 0, 0)", productCampaignPrice.getCssValue("text-decoration"));
             Assert.assertEquals("700", productCampaignPrice.getCssValue("font-weight"));
             Assert.assertEquals("18px", productCampaignPrice.getCssValue("font-size"));
 
             // д) акционная цена крупнее, чем обычная (это тоже надо проверить на каждой странице независимо)
             Assert.assertTrue(checkBiggerValue(
-                    productCampaignPrice.getCssValue("font-size"),
-                    productRegularPrice.getCssValue("font-size")
+                    stringToFloat(productCampaignPrice.getCssValue("font-size")),
+                    stringToFloat(productRegularPrice.getCssValue("font-size"))
                     )
             );
 
